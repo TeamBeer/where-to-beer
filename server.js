f238f69e6bf7ed2e98e5f0d1ff6564c980bd5602
Phil B <phil@berryman.org.uk>
2018-10-23 23:42:07 +0100

    added voting object to response for Get / Post Event(s)

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+// This function is shared by both the Post and Get routes for event
+const getEventFromDb = (eventId) => {
+  console.log('getEventFromDb')
+  return Promise.all([
+    db.one('SELECT * FROM event WHERE id = $1',[eventId]),
+    db.any('SELECT suggestion.venue_name, suggestion.reason, suggestion.postcode, member.name FROM suggestion, member WHERE event_id = $1 AND suggestion.member_id = member.id', [eventId]),
+    db.any('SELECT  event.id AS "eventId", vote.id AS "voteId" , suggestion.id AS "suggestionId", member.id AS "memberId", member.name AS "memberName" FROM vote, member, suggestion, event WHERE event_id = $1 AND vote.suggestion_id = suggestion.id AND event.id = suggestion.event_id AND member.id = suggestion.member_id GROUP BY event.id, suggestion.id, vote.id, member.name, member.id', [eventId])
+  ])
+  .then(([event,suggestions,votes])=> ({event:event,suggestions:suggestions,votes:votes})
+  )
+}
+
 
 // POST / INSERT :: New Event
 // This endpoint creates an entry in member, event, member_event and suggestion tables
@@ -41,16 +53,6 @@ app.post('/api/event', (req, res) => {
       res.json({error: error.message})
     })
   })
-
-const getEventFromDb = (eventId) => {
-    return Promise.all([
-      db.one('SELECT * FROM event WHERE id = $1',[eventId]),
-      db.any('SELECT suggestion.venue_name, suggestion.reason, suggestion.postcode, member.name FROM suggestion, member WHERE event_id = $1 AND suggestion.member_id = member.id', [eventId])
-    ])
-    .then(([event,suggestions])=> ({event:event,suggestions:suggestions})
-    )
-  }
-
 
 
 // PUT / UPDATE :: Event
@@ -75,12 +77,16 @@ app.put('/api/event/:eventId', (req, res) => {
 // This endpoint creates an object containing event details (from event table) and suggestions for that event (from suggestions table)
 app.get('/api/event/:eventId', (req, res) => {
   const eventId = req.params.eventId
-  Promise.all([
-    db.one('SELECT * FROM event WHERE id = $1',[eventId]),
-    db.any('SELECT suggestion.venue_name, suggestion.reason, suggestion.postcode, member.name FROM suggestion, member WHERE event_id = $1 AND suggestion.member_id = member.id', [eventId])
-  ])
-  .then(([event,suggestions])=> res.json({event:event,suggestions:suggestions}))
+  console.log(eventId)
+  getEventFromDb(eventId).then((eventData)=> {
+    console.log(eventData)
+    res.json(eventData)
   })
+  .catch((error)=>{
+    console.log('aaa' + eventId)
+    res.json({error: error.message})
+  })
+})
 
 
 // POST :: New Suggestion
@@ -109,7 +115,15 @@ app.post('/api/vote', (req, res) => {
 
 // DELETE :: Vote on Suggestion
 
-
+app.delete('/api/vote/:voteId', (req,res) => {
+  const voteId = req.params.voteId;
+  console.log('hello' + voteId)
+  db.none('DELETE FROM vote where id = $1', [voteId])
+  .then(()=> res.status(204).json({message: 'vote deleted'}))
+  .catch(error => {
+    res.json({error: error.message});
+  })
+})
 
 
 app.listen(8080, function(){

7db4117f46bd21f292d159e1a47f9420f9ebc4d1
danGRT <d.geraghty06@gmail.com>
2018-10-23 16:36:20 +0100

    fetch request in place, takes data from form (stored in state), posts to db

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ class App extends React.Component {
   this.createNewEvent = this.createNewEvent.bind(this)
 
   this.state = {
-    urlToShare: ""
+    urlToShare: "", //populated by createNewEvent when form is submitted
+    formData: {},   //populated by form submit
   }
-
 
   }
 
@@ -18,10 +18,9 @@ class App extends React.Component {
 
 // On page load, initial user/group starter will fill out form, and on form submit, will run the following function to post to database
   createNewEvent(){
-    //reference to formData being posted
     fetch('/api/event', {
       method: 'post',
-      body: JSON.stringify(formData),
+      body: JSON.stringify(this.state.formData),
       headers: {
         'Content-Type': 'application/json'
       })
@@ -38,78 +37,78 @@ class App extends React.Component {
 
   render() {
 
-return (
+    return (
 
-<main>
-  <header className="app__header">
-    <h1 className="app__title">Beer?</h1>
-  </header>
+        <main>
+          <header className="app__header">
+            <h1 className="app__title">Beer?</h1>
+          </header>
 
-{/* organiser-setup */}
+        {/* organiser-setup */}
 
-  <section className="organiserSetup">
-    <header className="setup__header">
-        <p>Organise at drink&hellip; all fields required unless stated</p>
-    </header>
+          <section className="organiserSetup">
+            <header className="setup__header">
+                <p>Organise at drink&hellip; all fields required unless stated</p>
+            </header>
 
-    <form className="setupform">
-            <div>
-                <label className="setupform__namelabel" htmlFor="name">Name</label>
-                <input className="setupform__name" type="text" name="name" value="" placeholder="NAME" required />
-            </div>
-            <div>
-                <label className="setupform__datelabel" htmlFor="date">Date</label>
-                <input className="setupform__date" type="date" name="date" value="" placeholder="DATE" required />
-            </div>
-            <div>
-                <label className="setupform__timelabel" htmlFor="time">Time</label>
-                <input className="setupform__time" type="time" name="time" value="" placeholder="TIME" required />
-            </div>
-            <div>
-                <label className="setupform__venuelabel" htmlFor="time">Pub Name</label>
-                    <input className="setupform__venue" type="text" name="venue" value="" placeholder="PUB NAME" required />
-            </div>
-            <div>
-                <label className="setupform__postcodelabel" htmlFor="postcode">Postcode</label>
-                <input className="setupform__postcode" type="text" name="postcode" value="" placeholder="POSTCODE" required />
-            </div>
-            <div>
-                <label className="ssetupform__commentlabel" htmlFor="comment">Postcode</label>
-                <textarea className="setupform__comment" name="comment" placeholder="Comment (optional)"></textarea>
-            </div>
-            <div>
-                <button className="btn btn__submit" type="submit" value="">Create</button>
-            </div>
-        </form>
-  </section>
+            <form className="setupform">
+                    <div>
+                        <label className="setupform__namelabel" htmlFor="name">Name</label>
+                        <input className="setupform__name" type="text" name="name" value="" placeholder="NAME" required />
+                    </div>
+                    <div>
+                        <label className="setupform__datelabel" htmlFor="date">Date</label>
+                        <input className="setupform__date" type="date" name="date" value="" placeholder="DATE" required />
+                    </div>
+                    <div>
+                        <label className="setupform__timelabel" htmlFor="time">Time</label>
+                        <input className="setupform__time" type="time" name="time" value="" placeholder="TIME" required />
+                    </div>
+                    <div>
+                        <label className="setupform__venuelabel" htmlFor="time">Pub Name</label>
+                            <input className="setupform__venue" type="text" name="venue" value="" placeholder="PUB NAME" required />
+                    </div>
+                    <div>
+                        <label className="setupform__postcodelabel" htmlFor="postcode">Postcode</label>
+                        <input className="setupform__postcode" type="text" name="postcode" value="" placeholder="POSTCODE" required />
+                    </div>
+                    <div>
+                        <label className="ssetupform__commentlabel" htmlFor="comment">Postcode</label>
+                        <textarea className="setupform__comment" name="comment" placeholder="Comment (optional)"></textarea>
+                    </div>
+                    <div>
+                        <button className="btn btn__submit" type="submit" value="">Create</button>
+                    </div>
+                </form>
+          </section>
 
-{/* organiser-confirmation */}
+        {/* organiser-confirmation */}
 
-  <section className="organiserConfirm">
+          <section className="organiserConfirm">
 
-    <header className="confirm__header">
-        <h2 className="confirm_title">Let's meet on Friday 26 October at 7pm</h2>
-    </header>
+            <header className="confirm__header">
+                <h2 className="confirm_title">Let's meet on Friday 26 October at 7pm</h2>
+            </header>
 
-    <section className="confirm__suggestion">
-      <h3>Your suggestion</h3>
-      <h4 className="suggestion_subtitle">The Star and Garter<span>W1F 7NX</span></h4>
-        <p className="suggestion_description">
-          It's got a nice  quiet room upstairs so we should get a seat
-        </p>
+            <section className="confirm__suggestion">
+              <h3>Your suggestion</h3>
+              <h4 className="suggestion_subtitle">The Star and Garter<span>W1F 7NX</span></h4>
+                <p className="suggestion_description">
+                  It's got a nice  quiet room upstairs so we should get a seat
+                </p>
 
-        <button className="btn btn__edit">Edit</button>
-        <button className="btn btn__submit" type="submit" value="">Get Invite Link</button>
+                <button className="btn btn__edit">Edit</button>
+                <button className="btn btn__submit" type="submit" value="">Get Invite Link</button>
 
-        <p className="confirm__viewlink">http://fancyapint.com/electric-dog</p>
-    </section>
+                <p className="confirm__viewlink">http://fancyapint.com/electric-dog</p>
+            </section>
 
-  </section>
-</main>
+          </section>
+        </main>
 
-    )
+            )
 
-  }
+          }
 
 }
 

46de63662870e38570a9e42f42914228c1ef3487
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-24 14:24:10 +0100

    vote button className change

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,37 +1,37 @@
 import React from "react";
 
-function Suggestion(){
+function Suggestion() {
 
-      return (
-        <React.Fragment>
+  return (
+    <React.Fragment>
 
-           <article className="suggestion__card suggestion--winner">
-              
-              <header className="suggestion_header">
-                  <h3 className="suggestion_title">
-                  <span className="suggestion_kicker">Joe suggests&hellip;</span>The Star and Garter
+      <article className="suggestion__card suggestion--winner">
+
+        <header className="suggestion_header">
+          <h3 className="suggestion_title">
+            <span className="suggestion_kicker">Joe suggests&hellip;</span>The Star and Garter
                   </h3>
-                  <button className="btn btn__add">+</button>
-              </header>
-              
-              <p className="suggestion_description">
-                  It's got a nice  quiet room upstairs so we should get a seat
+          <button className="btn btn__vote">+</button>
+        </header>
+
+        <p className="suggestion_description">
+          It's got a nice  quiet room upstairs so we should get a seat
               </p>
 
-              <footer className="suggestion_footer">
-                  <div className="suggestion_votes">
-                      <span className="suggestion_count">2</span> Votes
+        <footer className="suggestion_footer">
+          <div className="suggestion_votes">
+            <span className="suggestion_count">2</span> Votes
                   </div>
-                  <ul className="suggestion_voters">
-                      <li className="suggestion_voter">P</li>
-                      <li>M</li>
-                  </ul>
-              </footer>
-              
-          </article>    
+          <ul className="suggestion_voters">
+            <li className="suggestion_voter">P</li>
+            <li>M</li>
+          </ul>
+        </footer>
 
-        </React.Fragment>
-      )
-    }  
+      </article>
+
+    </React.Fragment>
+  )
+}
 
 export default Suggestion;
